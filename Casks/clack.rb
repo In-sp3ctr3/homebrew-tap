64f07e2f1db25a03576b98858e9f0907a7f8a94d
In-sp3ctr3 <spectrecodehub@gmail.com>
2026-05-19 17:44:26 -0500

    Test cask install path

--- Casks/clack.rb
+++ Casks/clack.rb
@@ -2,11 +2,14 @@ cask "clack" do
   version "0.1.0-alpha.6,2026-05-19"
   sha256 "699e363433c0498bf53bf08f659338c631396e6313cd1658c558dd3fc3934ef5"
 
-  url "https://github.com/In-sp3ctr3/clack/releases/download/v#{version.csv.first}/Clack-#{version.csv.first}-#{version.csv.second}.dmg",
-      verified: "github.com/In-sp3ctr3/clack/"
+  url "https://github.com/In-sp3ctr3/clack/releases/download/v#{version.csv.first}/Clack-#{version.csv.first}-#{version.csv.second}.dmg"
   name "Clack"
   desc "Native macOS clipboard history app"
   homepage "https://github.com/In-sp3ctr3/clack"
+
+  livecheck do
+    skip "Alpha builds are updated manually in this tap."
+  end
 
   depends_on macos: ">= :ventura"
 
